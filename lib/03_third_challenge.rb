fe2de8a5107836bfeb3fb2c6e320c46ef1aefe08
Penn M Diba <pdiba@oxbowschool.org>
2020-05-25 00:44:22 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -3,31 +3,31 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague",
-        :age => "53",
+        :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
