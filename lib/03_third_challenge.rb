e286934bf2922f9428a54cf846346d416347c809
Penn M Diba <pdiba@oxbowschool.org>
2020-05-25 00:27:25 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name =>
+        :age => 
         },
       :matriarch => {
         #your key/value pairs here
